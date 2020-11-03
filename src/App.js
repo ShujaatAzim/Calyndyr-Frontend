35bc5720ef731b14dc48b1b340cfd0350b38c824
Shujaat Azim <shujaat.azim@gmail.com>
2020-11-02 23:26:39 -0500

    Built out a tester post to see if I can create a day upon a day change, but not properly working. But close.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,26 @@ const App = () => {
 
   const [date, setDate] = useState()
 
+  const createDay = date => {
+    let d = date
+    fetch('http://localhost:3000/days', {
+      method: 'POST',
+      headers: {
+        Content: "application/json",
+        Accept: "application/json"
+      },
+      body: {
+        day: {
+          date: d
+        }
+      }
+    })
+    .then(resp => resp.json())
+    .then(console.log(d))
+  }
+
   return (
-    <div style={{ padding: "33%" }}>
+    <div style={{ paddingLeft: "33%", paddingRight: "33%" }}>
       <p>
         Selected date: {date ? format(date, 'dd MMM yyyy', { locale: enGB }) : 'none'}.
       </p>
